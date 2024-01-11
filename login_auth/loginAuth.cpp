c2c0ad9839ad82383c35e787ac1aeca9a9b60f78
Hunter C <notryoshi@gmail.com>
2024-01-11 04:03:54 -0800

    Feature. Almost completed login authentication

--- login_auth/loginAuth.cpp
+++ login_auth/loginAuth.cpp
@@ -1,55 +1,58 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-class User
-{
+class User {
 public:
   string username;
   string password;
 };
 
-int main()
-{
+int main() {
   User newUser;
-  // Need to add so much stuff. Database, hashing passwords, etc. Maybe even roles.
 
-  cout << "Welcome to a login authentication. Please create an account here: \n\n";
+  cout << "Welcome to a login authentication. Please create an account here: "
+          "\n\n";
   cout << "Username: ";
   cin >> newUser.username;
 
   cout << "\nPassword: ";
   cin >> newUser.password;
 
-  char str[75];
-
   string hashedPassword;
-  // Encrypting passwords. Adding 3 to each ASCII value
-  for (int i = 0; (i < 75 && str[i] != '\0'); i++)
-  {
-
-    str[i] = str[i] + 2;
-    hashedPassword += str[i];
+  // Encrypting passwords. Adding 2 to each ASCII value
+  for (int i = 0; i < newUser.password.length(); i++) {
+    newUser.password[i] = newUser.password[i] + 2;
+    hashedPassword += newUser.password[i];
   }
 
-  cout << "\nEncrypted Password: " << str << endl;
+  cout << "\nEncrypted Password: " << hashedPassword << endl;
   cout << "\nThank you for creating an account!\n";
+
+  string encryptedAttemptPassword;
+  // Encrypting the user's input password. Adding 2 to each ASCII value
+  for (int i = 0; i < newUser.password.length(); i++) {
+    encryptedAttemptPassword += newUser.password[i] + 2;
+  }
 
   string attemptUsername;
   string attemptPassword;
 
-  cout << "Login with username: ";
+  cout << "\nLogin with username: ";
   cin >> attemptUsername;
 
   cout << "\nNow enter password: ";
   cin >> attemptPassword;
 
-  if (newUser.username == attemptUsername && newUser.password == attemptPassword)
-  {
-    cout << "\nCongratulations, you have successfully logged in with the username: " << newUser.username;
-  }
-  else
-  {
+  if (newUser.username == attemptUsername && hashedPassword == encryptedAttemptPassword) {
+    cout << "\nCongratulations, you have successfully logged in with the "
+            "username: "
+         << newUser.username;
+  } else {
     cout << "\nInvalid information.";
   }
+
+  return 0;
 }
+
